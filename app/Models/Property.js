c99b1982f3fc71f5bd671e518ce82934d5989c15
Hugo Hernani <hhernanni@gmail.com>
2019-12-05 00:47:47 -0300

    Add nearBy that uses naval geolocalization calc to find nearest property

--- app/Models/Property.js
+++ app/Models/Property.js
@@ -3,6 +3,8 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
+const Database = use('Database')
+
 class Property extends Model {
   user(){
     return this.belongsTo('App/Models/User')
@@ -10,6 +12,21 @@ class Property extends Model {
 
   images(){
     return this.hasMany('App/Models/Image')
+  }
+
+  // Scopes
+  static scopeNearBy(query, latitude, longitude, distance) {
+    const haversine = `(6371 * acos(cos(radians(${latitude}))
+      * cos(radians(latitude))
+      * cos(radians(longitude)
+      - radians(${longitude}))
+      + sin(radians(${latitude}))
+      * sin(radians(latitude))))`
+
+
+    return query
+      .select('*', Database.raw(`${haversine} as distance`))
+      .whereRaw(`${haversine} < ${distance}`)
   }
 }
 
